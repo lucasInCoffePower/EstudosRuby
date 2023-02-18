93c60a12940b4fe4c371e6576bf5f8e1de9eedbd
YoursTrully (YT) <52331590+lucasInCoffePower@users.noreply.github.com>
2023-02-18 15:16:23 -0300

    Add files via upload

---
+++ programacao_estruturada/projeto1/projeto.rb
@@ -0,0 +1,14 @@
+
+
+
+puts "Bem-Vindo a biblioteca Ruby !"
+
+puts "Insira os dados para cadastro"
+
+print "Nome: "
+nome = gets.chomp
+
+print "Idade: "
+idade = gets.chomp.to_i
+
+puts "Seu nome é #{nome} e sua idade é #{idade}"
